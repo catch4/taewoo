8addb79716981dcb88be01710be84237a4c3d500
bn-tw2020 <skaxodn97@gmail.com>
2021-04-07 21:43:27 +0900

    [ADD] 현장풀이

---
+++ week4/21616.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <cstring>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+int arr[50001];
+int prefix_sum[50001];
+int dp[3][50001]; // 0,1,2 번째 소형기관차를 사용해 idx에서 최대 인구값
+
+void input() {
+    cin >> n;
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+    prefix_sum[0] = 0;
+    for(int i = 1; i <= n; i++) {
+        prefix_sum[i] = prefix_sum[i - 1] + arr[i - 1];
+    }
+    cin >> m;
+}
+
+int go(int small_train, int idx) {
+    
+    if(small_train > 3) return 0;
+    if(idx >= n) return 0;
+
+    int &ret = dp[small_train][idx];
+    if(ret != -1) return dp[small_train][idx];
+
+    // 현재 소형 기관차를 사용하지 않은경우..
+    // 소형 기관차를 사용하는 경우..
+    if(idx + m - 1 < n) {
+        ret = max(go(small_train, idx + 1), go(small_train + 1, idx + m) + prefix_sum[idx + m] - prefix_sum[idx]);
+    }
+    return ret;
+}
+
+void pro(){
+    memset(dp, -1, sizeof(dp));
+    cout << go(0, 0);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    input();
+    pro();
+    return 0;
+}
--- week4/2250.cpp
+++ week4/2250.cpp
@@ -58,6 +58,6 @@ int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(0);
     input();
-    pro(); 
+    pro();
     return 0;
 }
